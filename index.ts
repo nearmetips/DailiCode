8077df383e1e6e91408c0b393bb19b5295b618f6
Nearme <nearmetips@gmail.com>
2025-07-25 14:24:42 +0800

    Delete index.ts

--- index.ts
+++
@@ -1,3 +0,0 @@
-export { ElcAgent } from './packages/cli/src/api/index.js';
-export type { AgentConfig, AgentResult } from './packages/cli/src/api/types.js';
-export { AuthType } from 'daili-code-core';
